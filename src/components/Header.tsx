743868dda8ecc60c19edf9235338c3b50cfa4f3e
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-05-27 23:14:35 +0530

    Initial Commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,72 @@
+import { Link } from "react-router-dom";
+import {
+  FaSearch,
+  FaShoppingBag,
+  FaSignInAlt,
+  FaSignOutAlt,
+  FaUser,
+} from "react-icons/fa";
+import { useState } from "react";
+import { User } from "../types/types";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import toast from "react-hot-toast";
+interface PropsType{
+  user:User | null;
+}
+
+export const Header = ({user}:PropsType) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const logoutHandler = async () => {
+    try{
+      await signOut(auth);
+      toast.success("Sign out")
+
+      setIsOpen(false);
+    }catch(e){
+      toast.error("Failed to sign out")
+    }
+  };
+  return (
+    <nav className="header">
+      <Link onClick={() => setIsOpen(false)} to={"/"}>
+        HOME
+      </Link>
+      <Link onClick={() => setIsOpen(false)} to={"/search"}>
+        <FaSearch />
+      </Link>
+      <Link onClick={() => setIsOpen(false)} to={"/cart"}>
+        <FaShoppingBag />
+      </Link>
+
+      {user?._id ? (
+        <>
+          <button onClick={() => setIsOpen((prev) => !prev)}>
+            <FaUser />
+          </button>
+          <dialog open={isOpen}>
+            <div>
+              {user.role === "admin" && (
+                <Link onClick={() => setIsOpen(false)} to="/admin/dashboard">
+                  Admin
+                </Link>
+              )}
+
+              <Link onClick={() => setIsOpen(false)} to="/orders">
+                Orders
+              </Link>
+              <button onClick={logoutHandler}>
+                <FaSignOutAlt />
+              </button>
+            </div>
+          </dialog>
+        </>
+      ) : (
+        <Link to={"/login"}>
+          <FaSignInAlt />
+        </Link>
+      )}
+    </nav>
+  );
+};
---
+++ src/pages/Home.tsx
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom";
+import ProductCard from "../components/Product-card";
+import { useLatestProductsQuery } from "../redux/api/productAPI";
+import toast from "react-hot-toast";
+import { Skeleton } from "../components/loader";
+import { CartItem } from "../types/types";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/reducer/cartReducer";
+
+const Home = () => {
+  const { data, isLoading, isError } = useLatestProductsQuery("");
+
+  const dispatch=useDispatch();
+
+  const addToCardHandler = (cartItem:CartItem) => {
+    if(cartItem.stock < 1) return toast.error("Out of stock");
+
+    dispatch(addToCart(cartItem));
+    toast.success("Added to cart")
+  };
+
+  if (isError) toast.error("Cannot fetch the products");
+  return (
+    <div className="home">
+      <section></section>
+      <h1>
+        Latest Products
+        <Link to="/search" className="findmore">
+          More
+        </Link>
+      </h1>
+
+      <main>
+        {isLoading ? (
+          <Skeleton width="80vh" />
+        ) : (
+          data?.products.map((i) => (
+            <ProductCard
+              key={i._id}
+              productId={i._id}
+              name={i.name}
+              price={i.price}
+              stock={i.stock}
+              handler={addToCardHandler}
+              photo={i.photo}
+            />
+          ))
+        )}
+      </main>
+    </div>
+  );
+};
+
+export default Home;
